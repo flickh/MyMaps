2be18aed7f721f243a79414a7bb07c1bbefaf01b
Felicity H <felicityh15@gmail.com>
2021-10-31 23:35:08 -0700

    Maps

---
+++ app/src/main/java/edu/stanford/huangfe/mymaps/CreateMapsActivity.kt
@@ -0,0 +1,148 @@
+package edu.stanford.huangfe.mymaps
+
+import android.app.Activity
+import android.content.DialogInterface
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import edu.stanford.huangfe.mymaps.databinding.ActivityCreateMapsBinding
+import edu.stanford.huangfe.mymaps.models.Place
+import edu.stanford.huangfe.mymaps.models.UserMap
+
+class CreateMapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityCreateMapsBinding
+    private var markers: MutableList<Marker> = mutableListOf()
+    private var isSatellite = false;
+    private lateinit var fabChangeView: FloatingActionButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityCreateMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //fabChangeView = findViewById(R.id.fabChangeView)
+
+        supportActionBar?.title = intent.getStringExtra(EXTRA_MAP_TITLE)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        // makes a snackBar at the bottom that goes away
+        mapFragment.view?.let {
+            Snackbar.make(it, "Long press to add a marker!", Snackbar.LENGTH_INDEFINITE)
+                .setAction("OK", {})
+                .setActionTextColor(ContextCompat.getColor(this, android.R.color.white))
+                .show()
+        }
+
+        //fabChangeView.setOnClickListener {
+       //     changeMapView();
+        //}
+    }
+
+    //private fun changeMapView() {
+    //    if(isSatellite) {
+    //        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE)
+    //    } else {
+    //        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL)
+    //        isSatellite = true;
+    //    }
+    //}
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_create_map, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // check item is save menu option
+        if (item.itemId == R.id.miSave) {
+            if (markers.isEmpty()) {
+                Toast.makeText(this, "There must be at least one marker on the map", Toast.LENGTH_LONG).show()
+                return true
+            }
+            val places = markers.map { marker -> Place(marker.title, marker.snippet, marker.position.latitude, marker.position.longitude) }
+            val userMap = UserMap(intent.getStringExtra(EXTRA_MAP_TITLE).toString(), places)
+            val data = Intent()
+
+            data.putExtra(EXTRA_USER_MAP, userMap)
+            setResult(Activity.RESULT_OK, data)
+            finish() //finish createMap activity and go back to parent
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        mMap.setOnInfoWindowClickListener { markerToDelete ->
+            markers.remove(markerToDelete)
+            markerToDelete.remove()
+        }
+
+        mMap.setOnMapLongClickListener { latLng ->
+            showAlertDialog(latLng)
+        }
+
+        // Add a marker in Sydney and move the camera
+        val silVal = LatLng(37.4, -122.1)
+        //val sydney = LatLng(-34.0, 151.0)
+        // mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(silVal, 5f))
+    }
+
+    private fun showAlertDialog(latLng: LatLng) {
+        val placeFormView = LayoutInflater.from(this).inflate(R.layout.dialog_create_place,null)
+        val dialog = AlertDialog.Builder(this)
+            .setTitle("Create a marker")
+            .setView(placeFormView)
+            .setNegativeButton("Cancel", null)
+            .setPositiveButton("OK", null)
+            .show()
+        dialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener {
+            val title = placeFormView.findViewById<EditText>(R.id.LinearLayout).text.toString()
+            val description = placeFormView.findViewById<EditText>(R.id.etDescription).text.toString()
+            if (title.trim().isEmpty() || description.trim().isEmpty()) {
+                Toast.makeText(this, "Place must have non-empty title and description", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            val marker = mMap.addMarker(MarkerOptions().position(latLng).title(title).snippet(description))
+            markers.add(marker)
+            dialog.dismiss()
+        }
+
+    }
+}
+
---
+++ app/src/main/java/edu/stanford/huangfe/mymaps/MainActivity.kt
@@ -0,0 +1,171 @@
+package edu.stanford.huangfe.mymaps
+
+import android.app.Activity
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import edu.stanford.huangfe.mymaps.models.Place
+import edu.stanford.huangfe.mymaps.models.UserMap
+import java.io.*
+
+const val EXTRA_USER_MAP = "EXTRA_USER_MAP"
+const val EXTRA_MAP_TITLE = "EXTRA_MAP_TITLE"
+private const val FILENAME = "UserMaps.data"
+private const val REQUEST_CODE = 1234
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvMaps: RecyclerView
+    private lateinit var fabCreateMap: FloatingActionButton
+    private lateinit var userMaps: MutableList<UserMap>
+    private lateinit var mapAdapter: MapsAdapter
+    //lateinit var mGoogleMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rvMaps = findViewById(R.id.rvMaps)
+        fabCreateMap = findViewById(R.id.fabCreateMap)
+
+        //userMaps = generateSampleData().toMutableList();
+        //userMaps.addAll(deserializeUserMaps(this).toMutableList())
+        userMaps = deserializeUserMaps(this).toMutableList();
+
+        //for current location EXTENSION
+        //checkMyPermission()
+
+        // set layout manager on rv
+        rvMaps.layoutManager = LinearLayoutManager(this)
+        // set adapter on rv
+        mapAdapter = MapsAdapter(this, userMaps, object: MapsAdapter.OnClickListener {
+            override fun onItemClick(position: Int) {
+                // when user taps on view in RV, navigate to new activity
+                val intent = Intent(this@MainActivity, DisplayMapActivity:: class.java)
+                intent.putExtra(EXTRA_USER_MAP, userMaps[position])
+                startActivity(intent)
+                overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right)
+            }
+        })
+        rvMaps.adapter = mapAdapter
+
+        fabCreateMap.setOnClickListener {
+            showAlertDialog()
+        }
+    }
+
+    // fun checkMyPermission(googleMap: GoogleMap) {
+    //        mGoogleMap = googleMap
+    //        mGoogleMap.setMyLocationEnabled(true)
+    //}
+
+    private fun showAlertDialog() {
+        val mapFormView = LayoutInflater.from(this).inflate(R.layout.dialog_create_map,null)
+        val dialog = AlertDialog.Builder(this)
+            .setTitle("Map title")
+            .setView(mapFormView)
+            .setNegativeButton("Cancel", null)
+            .setPositiveButton("OK", null)
+            .show()
+        dialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener {
+             val title = mapFormView.findViewById<EditText>(R.id.etTitle).text.toString()
+            if (title.trim().isEmpty()) {
+                Toast.makeText(this, "Place must have non-empty title", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+            // navigate to create map activity
+            val intent = Intent(this@MainActivity, CreateMapsActivity::class.java)
+            intent.putExtra(EXTRA_MAP_TITLE, title)
+            startActivityForResult(intent, REQUEST_CODE)
+            dialog.dismiss()
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            // get new map data from data
+            val userMap = data?.getSerializableExtra(EXTRA_USER_MAP) as UserMap
+            userMaps.add(userMap)
+            mapAdapter.notifyItemInserted(userMaps.size-1)
+            serializeUserMaps(this, userMaps)
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun serializeUserMaps(context: Context, userMaps: List<UserMap>) {
+        ObjectOutputStream(FileOutputStream(getDataFile(context))).use {
+            it.writeObject(userMaps) //writes out some binary data, breakes it down into basic component
+        }
+    }
+
+    private fun deserializeUserMaps(context: Context) : List<UserMap> {
+        val dataFile = getDataFile(context)
+        if (!dataFile.exists()) { //data file dne yet
+            return emptyList()
+        }
+        ObjectInputStream(FileInputStream(dataFile)).use { return it.readObject() as List<UserMap> }
+    }
+
+    private fun getDataFile(context : Context) : File {
+        Log.i(TAG, "Getting file from directory ${context.filesDir}")
+        return File(context.filesDir, FILENAME)
+    }
+    private fun generateSampleData(): List<UserMap> {
+        return listOf(
+            UserMap(
+                "Memories from University",
+                listOf(
+                    Place("Branner Hall", "Best dorm at Stanford", 37.426, -122.163),
+                    Place("Gates CS building", "Many long nights in this basement", 37.430, -122.173),
+                    Place("Pinkberry", "First date with my wife", 37.444, -122.170)
+                )
+            ),
+            UserMap("January vacation planning!",
+                listOf(
+                    Place("Tokyo", "Overnight layover", 35.67, 139.65),
+                    Place("Ranchi", "Family visit + wedding!", 23.34, 85.31),
+                    Place("Singapore", "Inspired by \"Crazy Rich Asians\"", 1.35, 103.82)
+                )),
+            UserMap("Singapore travel itinerary",
+                listOf(
+                    Place("Gardens by the Bay", "Amazing urban nature park", 1.282, 103.864),
+                    Place("Jurong Bird Park", "Family-friendly park with many varieties of birds", 1.319, 103.706),
+                    Place("Sentosa", "Island resort with panoramic views", 1.249, 103.830),
+                    Place("Botanic Gardens", "One of the world's greatest tropical gardens", 1.3138, 103.8159)
+                )
+            ),
+            UserMap("My favorite places in the Midwest",
+                listOf(
+                    Place("Chicago", "Urban center of the midwest, the \"Windy City\"", 41.878, -87.630),
+                    Place("Rochester, Michigan", "The best of Detroit suburbia", 42.681, -83.134),
+                    Place("Mackinaw City", "The entrance into the Upper Peninsula", 45.777, -84.727),
+                    Place("Michigan State University", "Home to the Spartans", 42.701, -84.482),
+                    Place("University of Michigan", "Home to the Wolverines", 42.278, -83.738)
+                )
+            ),
+            UserMap("Restaurants to try",
+                listOf(
+                    Place("Champ's Diner", "Retro diner in Brooklyn", 40.709, -73.941),
+                    Place("Althea", "Chicago upscale dining with an amazing view", 41.895, -87.625),
+                    Place("Shizen", "Elegant sushi in San Francisco", 37.768, -122.422),
+                    Place("Citizen Eatery", "Bright cafe in Austin with a pink rabbit", 30.322, -97.739),
+                    Place("Kati Thai", "Authentic Portland Thai food, served with love", 45.505, -122.635)
+                )
+            )
+        )
+    }
+
+}
